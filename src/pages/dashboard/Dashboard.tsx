b189c7989a9b1b9e71a72325164c2cf2e2af8121
iljoung oh <70251881+happy8131@users.noreply.github.com>
2024-04-30 23:56:36 +0900

    cart component add

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Box, Grid, Paper } from "@mui/material";
 import scss from "./Dashboard.module.scss";
 import DataRibbon from "@/components/Dashboard/DataRibbon";
+import TransactionsPerDay from "@/components/Dashboard/TransactionsPerDay";
 //dd
 const Dashboard = () => {
   return (
     <Box>
       <DataRibbon />
+      <TransactionsPerDay />
     </Box>
   );
 };
